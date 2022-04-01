618a19f8e02f3e5e11ae833b6ec7e0169d5a8f46
weqqr <weqqr@yandex.ru>
2022-04-01 22:49:50 +0300

    Implement CRS for isometric projection

--- static/panorama.js
+++ static/panorama.js
@@ -1,7 +1,44 @@
+let crs = L.extend(L.CRS.Simple, {
+    projection: {
+        project: latlng => {
+            let x = (latlng.lng - latlng.lat) * 16 / 2;
+            let y = (latlng.lng + latlng.lat) * 16 / 4;
+            return new L.Point(x, y);
+        },
+
+        unproject: point => {
+            point.y *= 2;
+            let lat = (point.y - point.x) / 16;
+            let lng = (point.y + point.x) / 16;
+            return new L.LatLng(lat, lng);
+        }
+    },
+    transformation: new L.Transformation(1, 0, 1, 0),
+});
+
+L.Control.CursorPosition = L.Control.extend({
+    options: {
+        position: 'topleft',
+    },
+    onAdd: map => {
+        return L.DomUtil.create('div', 'cursor-position leaflet-control leaflet-bar');
+    }
+});
+
 let map = L.map('map', {
     attributionControl: false,
-    crs: L.CRS.Simple,
-}).setView([0, 0], 0);
+    crs: crs,
+}).setView([735, -818], 0);
+
+let position = new L.Control.CursorPosition();
+position.addTo(map);
+
+map.on('click mouseover mousemove', e => {
+    let x = e.latlng.lat;
+    let z = e.latlng.lng;
+    position.getContainer().innerHTML = `X=${x.toFixed()} Y=0 Z=${z.toFixed()}`;
+});
+
 L.tileLayer('/tiles/{z}/{x}/{y}.png', {
     maxZoom: 0,
     minZoom: -7,
